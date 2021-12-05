fc59d44b1b4a8cca5818f04a6e9ce48f90f6ea6b
Catherine Celice <CCelice@bcbsm.com>
2021-12-05 10:03:59 -0500

    working user database backend functions.  Get still needs to be double=checked. Also, these now need to be integrated with the frontend.  Slight changes to user.ts to make more attributes optional.

--- functions/src/routes/userDBaccessRoutes.ts
+++ functions/src/routes/userDBaccessRoutes.ts
@@ -1,19 +1,22 @@
-
+// 2 errors in the get code.
 
 // require the express module
 import express from "express";
-import { ObjectId } from "mongodb";
+
 import { getClient } from "../db";
 import User from "../models/user";
- 
+import { ObjectId, OptionalId  } from "mongodb";
+//import { ObjectId } from "mongodb";
 // create a new Router object
 const userRoutes = express.Router();
  
+// Functions for use in our application
+// functions for our personal use during development appear after these
 
-
+// tested and it works
 userRoutes.post("/users", async (req, res) => {
-     const newUser = req.body as User;
-     // const newUser = req.body as OptionalId<User>;
+     //const newUser = req.body as User;
+     const newUser = req.body as OptionalId<User>;
      try {
         const client = await getClient();
         // not sure if Project_3 needs to be or should be there
@@ -28,7 +31,7 @@ userRoutes.post("/users", async (req, res) => {
      }
 });
 
-
+// tested and it works
 userRoutes.get("/users", async (req, res) => {
     const dbquery: any = {};
     try{
@@ -47,14 +50,33 @@ userRoutes.get("/users", async (req, res) => {
 
 });
 
+  // This function is the one we will use to get a single registered user.
+  userRoutes.get("/users/:mail/:password1", async (req, res) => {
+    const m = req.params.mail;
+    const pwd = req.params.password1;
+    try {
+      const client = await getClient();
+      const user = await client.db("Project_3").collection<User>('users').findOne({ email : m, password1: pwd });
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({message: "Not Found"});
+      }
+    } catch (err) {
+      console.error("FAIL", err);
+      res.status(500).json({message: "Internal Server Error"});
+    }
+  });
 
 
-userRoutes.delete("/users/:id", async (req, res) => {
-    const id = req.params.id;
+// tested and works -- this is how we should delete users - by email and password combo
+userRoutes.delete("/users/:mail/:password1", async (req, res) => {
+    const m = req.params.mail;
+    const pwd = req.params.password1;
     try {
         const client = await getClient();
-        const result = await client.db().collection<User>('users')
-        .deleteOne({ _id: new ObjectId(id) });
+        const result = await client.db("Project_3").collection<User>('users')
+        .deleteOne({ email: m, password1: pwd});
         if (result.deletedCount === 0) {
         } else {
             res.status(204).end();
@@ -66,12 +88,82 @@ userRoutes.delete("/users/:id", async (req, res) => {
 
 
 
+//tester code
+userRoutes.put("/users/:firstname", async (req, res) => {
+    
+    const first = req.params.firstname;
+    try {
+        const client = await getClient();
+        const user = req.body as OptionalId<User>;
+        const results = await client.db("Project_3").collection<User>('users')
+        .replaceOne({firstname: first}, user);
+        if(results.modifiedCount === 0 ) {
+            res.status(404)
+          }
+          else {
+            res.send(200);
+          }
+     
+    } catch (err) { 
+        console.log("");
+        res.status(500);
+    }
+});
+
+// Functions for use by us during development
+
+
+// This function is for our own use, only, so that we can make quick changes to the database for testing purposes.
+// tested and it works - but cannot have with the email get below
+userRoutes.get("/users/:firstname", async (req, res) => {
+  const first = req.params.firstname;
+  try {
+    const client = await getClient();
+    const user = await client.db("Project_3").collection<User>('users').findOne({ firstname : first });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({message: "Not Found"});
+    }
+  } catch (err) {
+    console.error("FAIL", err);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+});
+
+// tested and it works, but it cannot be operational while the get firstname is also available (above)
+// userRoutes.get("/users/:email", async (req, res) => {
+//   const mail = req.params.email;
+//   try {
+//     const client = await getClient();
+//     const user = await client.db("Project_3").collection<User>('users').findOne({ email : mail });
+//     if (user) {
+//       res.json(user);
+//     } else {
+//       res.status(404).json({message: "Not Found"});
+//     }
+//   } catch (err) {
+//     console.error("FAIL", err);
+//     res.status(500).json({message: "Internal Server Error"});
+//   }
+// });
+
+// This works, but do we want to delete by id? -- no
+userRoutes.delete("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+      const client = await getClient();
+      const result = await client.db("Project_3").collection<User>('users')
+      .deleteOne({ _id: new ObjectId(id) });
+      if (result.deletedCount === 0) {
+      } else {
+          res.status(204).end();
+      }
+  } catch (err) { console.log("delete error") }
+});
+
+
 export default userRoutes;
-
-
-// function async(req: any, res: any): import("express-serve-static-core").RequestHandler<{}, any, any, import("qs").ParsedQs, Record<string, any>> {
-//     throw new Error("Function not implemented.");
-// }
 
 
 
